3cfb00d0e242db52b93548235a44f15cea61c926
Mikko Vatanen <mikko.vatanen@csc.fi>
2013-10-17 16:44:18 +0300

    KDKPAS-157 Moved schematron tests to correct place. Added common paths to testcommon.settings (USE THEM!). Fixed some paths in tests

--- tests/testcommon/settings.py
+++ tests/testcommon/settings.py
@@ -1,16 +1,37 @@
+"""Define common settings for all tests such as:
+
+    * Override Python sys.path:
+        * src
+        * tests/testcommon
+
+    * Provide sane directory names for all tests:
+        * PROJECT
+        * DIRTESTSDIR
+        * SOURCEDIR
+        * INCLUDEDIR
+        * TESTDATADIR
+    
+"""
+
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                '../../src')))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                '../../tests')))
 
-TOOLSPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+# Defined some useful directories
+PROJECTDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+assert PROJECTDIR.endswith('information-package-tools')
+
+TESTSDIR = os.path.join(PROJECTDIR, 'tests')
+SOURCEDIR = os.path.join(PROJECTDIR, 'src')
+INCLUDEDIR = os.path.join(PROJECTDIR, 'include')
+SHAREDIR = os.path.join(PROJECTDIR, 'include/share')
+
+sys.path.insert(0, SOURCEDIR)
+sys.path.insert(0, TESTSDIR)
+
 # Check this when having problems loading modules
 print sys.path
 
-TESTDATAPATH = os.path.join(
-    os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')),
-    'tests/data')
+TESTDATADIR = os.path.join(TESTSDIR, 'data')
+
 print os.path.dirname(__file__)
-print 'testdatapath = ', TESTDATAPATH
+print 'testdatapath = ', TESTDATADIR
