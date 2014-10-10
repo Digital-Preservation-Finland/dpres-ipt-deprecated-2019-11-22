daf5f6081906aa66c18ae82724e46ac2946b24b3
Ville-Pekka Vainio <ville-pekka.vainio@csc.fi>
2014-10-10 16:20:55 +0300

    KDKPAS-580 autopep8 fixes for HTML report

--- information-package-tools/scripts/create_html_report.py
+++ information-package-tools/scripts/create_html_report.py
@@ -35,23 +35,22 @@ def main(arguments=None):
         return 1
 
     if args[0] is None:
-    	return 1
+        return 1
     if args[1] is not None:
-    	html_path = args[1]
+        html_path = args[1]
     else:
-    	report_name = os.path.basename(args[0])
-    	html_path = os.path.join(os.path.dirname(args[0]), report_name)
+        report_name = os.path.basename(args[0])
+        html_path = os.path.join(os.path.dirname(args[0]), report_name)
 
     cmd = "xsltproc " + XSLT_PATH + " " + args[0]
     print cmd
     cmd = shlex.split(cmd)
-    p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stderr=
-                             subprocess.PIPE, stdout=subprocess.PIPE,
-                             close_fds=True, shell=False)
+    p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE,
+                         close_fds=True, shell=False)
     (stdout, stderr) = p.communicate()
     if len(stderr) > 0:
-       print "xsltproc had stderr output:"
-       print stderr
+        print "xsltproc had stderr output:"
+        print stderr
 
     if len(stdout) > 0:
         with open(html_path, "w") as html_file:
@@ -62,4 +61,3 @@ def main(arguments=None):
 
 if __name__ == '__main__':
     main()
-
