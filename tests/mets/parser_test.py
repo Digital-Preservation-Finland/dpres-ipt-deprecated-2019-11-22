81d8bde00dbb64e552af1f15b86d5449aea0fbbc
Ville-Pekka Vainio <ville-pekka.vainio@csc.fi>
2015-06-24 15:59:53 +0300

    KDKPAS-940 Also test a file name which does not actually need unquoting. Add better error messages

--- tests/mets/parser_test.py
+++ tests/mets/parser_test.py
@@ -7,7 +7,6 @@ import sys
 # Needed for testcommon.settings
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-import pytest
 import testcommon.settings
 
 # Module to test
@@ -21,11 +20,18 @@ class TestMetsParser:
     """Tests for the ipt.mets.parser module"""
 
     def test_get_file_location(self):
-        """Test the get_file_location method"""
+        """Test the get_file_location method by getting both a URL with
+        international characters and a URL without them"""
         mets_file = os.path.join(METSDIR, "mets.xml")
         lxml = ipt.mets.parser.LXML(filename=mets_file)
-        first_file = lxml.mets_files()[0]
+        mets_files = lxml.mets_files()
+        first_file = mets_files[0]
+        second_file = mets_files[1]
         first_file_location = lxml.get_file_location(first_file)
-        print first_file_location
+        second_file_location = lxml.get_file_location(second_file)
         assert first_file_location == \
-            "file://asettamiset/122390_OPM_päätös_20.05.2008_12-02-42.pdf"
+            "file://asettamiset/122390_OPM_päätös_20.05.2008_12-02-42.pdf", \
+            "It seems there was a problem with URL unquoting a file name"
+        assert second_file_location == \
+            "file://asettamiset/128343_Vihanta.pdf", \
+            "It seems there was a problem with URL unquoting a file name"
