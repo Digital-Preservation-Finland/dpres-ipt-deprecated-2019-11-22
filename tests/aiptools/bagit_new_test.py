aa6978bf1bc6f838aba06adab1ef51c59c43cd94
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2015-08-14 13:12:50 +0300

    KDKPAS-954 Added new tests for main and file writing.

--- tests/aiptools/bagit_new_test.py
+++ tests/aiptools/bagit_new_test.py
@@ -5,8 +5,11 @@ This is a test module for bagit.py
 import os
 import tempfile
 
+import pytest
 
-from ipt.aiptools.bagit_new import make_manifest, calculate_md5
+from ipt.aiptools import bagit_new
+from ipt.aiptools.bagit_new import make_manifest, calculate_md5, main, \
+    write_manifest, BagitError
 
 
 def test_make_manifest():
@@ -36,3 +39,32 @@ def test_make_manifest():
     assert manifest == [
         ['e2fc714c4727ee9395f324cd2e7f331f', 'data/file_1.txt'],
         ['e80b5017098950fc58aad83c8c14978e', 'data/kuvat/image1.jpg']]
+
+
+def test_main(monkeypatch):
+    """Testing main function."""
+    monkeypatch.setattr(bagit_new, 'make_manifest', lambda manifest: True)
+    monkeypatch.setattr(
+        bagit_new, 'write_manifest', lambda manifest, path: True)
+    main(argv=['bagit.py', 'make_bag', 'foo'])
+
+    with pytest.raises(BagitError):
+        main(argv=['bagit.py', 'make_bag'])
+
+    with pytest.raises(BagitError):
+        main(argv=['bagit.py', 'foo', 'bar'])
+
+
+def test_write_manifest():
+    tmpdir = tempfile.mkdtemp(prefix="tests.testpath.")
+    sip_dir = os.path.join(tmpdir, 'sip')
+    os.makedirs(sip_dir)
+    manifest = [
+        ['ab123', os.path.join(sip_dir, 'file.txt')],
+        ['ab1232', os.path.join(sip_dir, 'file2.txt')]]
+    write_manifest(manifest, sip_dir)
+    with open(os.path.join(sip_dir, 'manifest-md5.txt'), 'r') as infile:
+        lines = infile.readlines()
+        print "lines", lines
+        assert lines[0] == 'ab123 ' + os.path.join(sip_dir, 'file.txt') + '\n'
+        assert lines[1] == 'ab1232 ' + os.path.join(sip_dir, 'file2.txt') + '\n'
