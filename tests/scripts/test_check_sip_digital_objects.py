b945a3be3ab9961d291396124d8dade55c241892
Juha Törnroos <juha.tornroos@csc.fi>
2015-08-17 13:10:55 +0300

    KDKPAS-945 Modified test_check_sip_digital_objects to validate that (std) output conforms Premis schema.

--- tests/scripts/test_check_sip_digital_objects.py
+++ tests/scripts/test_check_sip_digital_objects.py
@@ -2,11 +2,17 @@ import os
 import sys
 import re
 import pytest
+import uuid
+import subprocess
 
 import testcommon.shell
 import testcommon.settings
 from testcommon.casegenerator import pytest_generate_tests
 
+from tempfile import NamedTemporaryFile
+import ipt.validator.plugin.xmllint
+
+import ipt.scripts.check_xml_schema_features
 # Module to test
 import ipt.scripts.check_sip_digital_objects
 
@@ -61,39 +67,72 @@ class TestCommandLineTools:
         }]
     }
 
+
+    # NOTE: This code was originally taken from preservation repository
+    # tests/asserts.py file and was refactored to use direct main() call
+    # instead of popen().
+    def premis_schema_validates(self, filename):
+        """Test that the given task-report or ingestion-report validates against
+        XML schema.
+
+        :filename: Report filename
+        :returns: True when report validates, else False
+
+        """
+        # Validate report againts XSD schema
+
+        # TODO: replace these after schemas have been refactored into
+        # information-package-tools
+
+        xsd_path = '/usr/share/pas/microservice/report/' \
+            'validation/premis_report.xsd'
+
+        arguments = ['-s%s' % xsd_path, filename]
+
+        (returncode, stdout, r) = testcommon.shell.run_main(
+            ipt.scripts.check_xml_schema_features.main, arguments)
+
+        print r
+        return returncode == 0
+
+
+
     def test_check_sip_digital_objects(self, testcase,
                                        filename, expected_result):
 
         filename = os.path.join(testcommon.settings.TESTDATADIR, 'test-sips',
                                 filename)
 
-        configfile = os.path.abspath(os.path.join(testcommon.settings.PROJECTDIR,
-                                                  'include/share',
-                                                  'validators',
-                                                  'validators.json'))
+        configfile = os.path.abspath(os.path.join(
+            testcommon.settings.PROJECTDIR,
+            'include/share/validators/validators.json'))
 
-        arguments = ["%s" % filename, "-c%s" % configfile, "xyz", "abc"]
-
-        self.do(ipt.scripts.check_sip_digital_objects.main, arguments,
-                expected_result)
-
-    def do(self, command, arguments, expected):
+        arguments = ["%s" % filename, "-c%s" % configfile, "preservation-sip-id"
+                     , str(uuid.uuid4())]
 
         (returncode, stdout, stderr) = testcommon.shell.run_main(
-            command, arguments)
+            ipt.scripts.check_sip_digital_objects.main, arguments)
 
         print stdout
         print >> sys.stderr, stderr
 
-        function_name = "%s.%s" % (command.__module__, command.func_name)
-        for match_string in expected["stdout"]:
+        for match_string in expected_result["stdout"]:
             assert match_string in stdout
 
-        for match_string in expected["stderr"]:
+        for match_string in expected_result["stderr"]:
             assert match_string in stderr
 
         message = "\n".join(["got:", str(returncode), "expected:",
-                             str(expected["returncode"]
-                                 ), "stdout:", stdout, "stderr:",
-                             stderr, "function:", function_name])
-        assert returncode == expected["returncode"], message
+                             str(expected_result["returncode"]
+                                 ), "stdout:", stdout, "stderr:"])
+
+        assert returncode == expected_result["returncode"], message
+
+        premis_fd = NamedTemporaryFile(delete=False)
+        try:
+            premis_fd.write(stdout)
+            premis_fd.close()
+
+            assert self.premis_schema_validates(premis_fd.name)
+        finally:
+            os.remove(premis_fd.name)
