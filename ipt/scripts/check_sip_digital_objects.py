729db09ddf8e84c514abc81b781c3176c5f57d6d
Juha Törnroos <juha.tornroos@csc.fi>
2015-08-17 16:01:32 +0300

    KDKPAS-945 Cleaned check-sip-digital-objects code.

--- ipt/scripts/check_sip_digital_objects.py
+++ ipt/scripts/check_sip_digital_objects.py
@@ -7,16 +7,16 @@ import optparse
 import ipt.validator.filelist
 import ipt.mets.parser
 import ipt.version
-from ipt.premis import premis
+from ipt.premis import premis as p
 
 
 def main(arguments=None):
 
-    usage = "usage: %prog <sip-path> <linking-sip-object-id>"
+    usage = "usage: %prog <sip-path> <linking-sip-object-type> " \
+        "<linking-sip-object-id>"
     parser = optparse.OptionParser(usage=usage)
     parser.add_option("-c", "--configfile", dest="config_filename",
-                      default=None,
-                      help="JSON configuration file",
+                      default=None, help="JSON configuration file",
                       metavar="PATH")
     (options, args) = parser.parse_args(arguments)
 
@@ -25,65 +25,63 @@ def main(arguments=None):
         return 1
 
     sip_path = args[0]
+    linking_sip_type = args[1]
+    linking_sip_id = args[2]
+
     mets_filename = os.path.abspath(os.path.join(sip_path, 'mets.xml'))
     basepath = os.path.abspath(os.path.dirname(mets_filename))
-    linking_sip_id = args[2]
-    linking_sip_type = args[1]
 
-    validate = ipt.validator.filelist.Validator(basepath)
-    validate.load_config(options.config_filename)
+    validator = ipt.validator.filelist.Validator(basepath)
+    validator.load_config(options.config_filename)
     mets_parser = ipt.mets.parser.LXML(mets_filename)
     filelist = mets_parser.get_fileinfo_iterator()
 
-    statuses, reports, errors, validators = validate.validate_files(filelist)
+    validation_results = validator.validate_files(filelist)
+    mets_filelist = mets_parser.get_fileinfo_array()
 
     return_status = 0
-    return_message = ""
+    report = p.Premis()
 
-    prem = premis.Premis()
+    for fileinfo, statuscode, stdout, stderror, _validator in \
+            zip(mets_filelist, *validation_results):
 
-    filelist = mets_parser.get_fileinfo_array()
+        validated_file = ipt.validator.filelist.FileInfo(fileinfo)
+        validated_file.from_dict(fileinfo)
 
-    for fileinfo, status, report, error, _validator in \
-            zip(filelist, statuses, reports, errors, validators):
-
-        if not _validator:
-            return_status = 1
-        elif status != 0:
-            return_status = 117
-
-        ff = ipt.validator.filelist.FileInfo(fileinfo)
-        ff.from_dict(fileinfo)
-
-        related_object = premis.Object()
+        related_object = p.Object()
         related_object.identifier = ""
         related_object.identifierType = linking_sip_type
         related_object.identifierValue = linking_sip_id
 
-        agent_name = str(
-            os.path.basename(__file__)) + "-" + ipt.version.__version__
-
-        linking_agent = premis.Agent()
-        linking_agent.fromvalidator(agentIdentifierType="preservation-agent-id",
-                                    agentIdentifierValue=agent_name,
-                                    agentName=agent_name)
+        linking_agent = p.Agent()
+        agent_name = "%s-%s" % (__file__, ipt.version.__version__)
+        linking_agent.fromvalidator(
+            agentIdentifierType="preservation-agent-id",
+            agentIdentifierValue=agent_name,
+            agentName=agent_name)
         linking_agent.type = "software"
-        prem.insert(linking_agent)
+        report.insert(linking_agent)
 
-        linking_object = premis.Object()
-        linking_object.fromvalidator(fileinfo=ff, relatedObject=related_object)
+        linking_object = p.Object()
+        linking_object.fromvalidator(
+            fileinfo=validated_file,
+            relatedObject=related_object)
 
-        validation_event = premis.Event()
-        validation_event.fromvalidator(status, report, error,
-                                       linkingObject=linking_object,
-                                       linkingAgent=linking_agent)
+        validation_event = p.Event()
+        validation_event.fromvalidator(
+            statuscode, stdout, stderror,
+            linkingObject=linking_object,
+            linkingAgent=linking_agent)
 
-        prem.insert(linking_object)
-        prem.insert(validation_event)
+        report.insert(linking_object)
+        report.insert(validation_event)
 
-    return_message = prem.serialize()
-    sys.stdout.write(return_message)
+        if not _validator:
+            return_status = 1
+        elif statuscode != 0:
+            return_status = 117
 
+    sys.stdout.write(report.serialize())
     return return_status
 
 if __name__ == '__main__':
