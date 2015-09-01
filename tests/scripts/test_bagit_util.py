c8c33f5a6f2669ce8b2bdb0d2c37d24ff648c9f4
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2015-09-01 13:33:52 +0300

    KDKPAS-954 Removed stale test sip generation and it's usage from code.

--- tests/scripts/test_bagit_util.py
+++ tests/scripts/test_bagit_util.py
@@ -6,14 +6,6 @@ import ipt.aiptools.bagit_new
 from ipt.scripts.bagit_util import main
 
 
-def create_test_sip(sip_path):
-    """Create test sip"""
-    os.makedirs(sip_path)
-    mets_path = os.path.join(sip_path, 'mets.xml')
-    with open(mets_path, 'w') as mets:
-        mets.write('asfasdfasdfsda')
-
-
 def test_main(testpath, monkeypatch):
     """test cases for main funtions commandline interface."""
 
@@ -27,6 +19,3 @@ def test_main(testpath, monkeypatch):
     os.makedirs(no_bagit_dir)
     with pytest.raises(BagitError):
         main(['prog', 'make_bag', no_bagit_dir])
-    sip_path = os.path.join(testpath, 'sippi')
-    create_test_sip(sip_path)
-    assert main(['prog', 'make_bag', sip_path]) == 0
