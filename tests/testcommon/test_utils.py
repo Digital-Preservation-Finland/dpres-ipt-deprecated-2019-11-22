ee7207bb3b2709940eb49becc3bd6f01d0ebb383
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2015-09-01 13:43:18 +0300

    KDKPAS-954 Added test bagit generation to test_utils.

--- tests/testcommon/test_utils.py
+++ tests/testcommon/test_utils.py
@@ -54,3 +54,32 @@ class Utils:
             os.makedirs(directory)
 
         return directory
+
+
+def create_test_bagit(bagit_path):
+    """Create test bagit."""
+    sip_path = os.path.join(bagit_path, 'data', 'transfers', 'sippi')
+
+    os.makedirs(sip_path)
+
+    mets_path = os.path.join(sip_path, 'mets.xml')
+    with open(mets_path, 'w') as mets:
+        mets.write('asfasdfasdfsda')
+
+    image_path = os.path.join(sip_path, 'kuvat')
+    os.makedirs(image_path)
+
+    file_1_path = os.path.join(sip_path, 'file_1.txt')
+    file_2_path = os.path.join(image_path, 'image1.jpg')
+    with open(file_1_path, 'w') as infile:
+        infile.write('abcd')
+    with open(file_2_path, 'w') as infile:
+        infile.write('abcdef')
+
+    with open(os.path.join(bagit_path, 'manifest-md5.txt'), 'w') as outfile:
+        outfile.write('e2fc714c4727ee9395f324cd2e7f331f ' + os.path.join(
+            'data', 'file.txt') + '\n')
+        outfile.write('e80b5017098950fc58aad83c8c14978e ' + os.path.join(
+            'data', 'kuvat', 'file2.txt') + '\n')
+    with open(os.path.join(bagit_path, 'bagit.txt'), 'w') as outfile:
+        outfile.write('foo\n')
