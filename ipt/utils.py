1ca994234d81b6ae2d204340d3f3422265c1fc54
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2015-12-01 18:00:56 +0200

    KDKPAS-1112 Removed stale code.

--- ipt/utils.py
+++ ipt/utils.py
@@ -1,6 +1,5 @@
 """Utility functions."""
 
-import os
 import subprocess
 
 
@@ -9,7 +8,6 @@ def run_command(cmd, stdout=subprocess.PIPE):
 
     :returns: Tuple (statuscode, stdout, stderr)
     """
-    #environment = os.environ.copy()
     proc = subprocess.Popen(cmd,
                             stdout=stdout,
                             stderr=subprocess.PIPE,
@@ -17,10 +15,5 @@ def run_command(cmd, stdout=subprocess.PIPE):
 
     (stdout, stderr) = proc.communicate()
     statuscode = proc.returncode
-    print ":"
-    print stdout
-    print stderr
-    print statuscode
-    print ":"
 
     return statuscode, stdout, stderr
