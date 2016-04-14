0e88b85bdb5e1bdd0318ca3f8d715cb4682494b7
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2016-04-14 16:42:51 +0300

    KDKPAS-1281 Test for addml.to_dict.

--- tests/mets/techmd_test.py
+++ tests/mets/techmd_test.py
@@ -3,7 +3,7 @@ import lxml.etree
 
 from tests.testcommon.settings import PROJECTDIR
 from ipt.mets.techmd import get_tech_md_list_for_file, \
-    _get_adm_id_for_file, _get_adm_id_attribute_string
+    _get_adm_id_for_file, _get_adm_id_attribute_string, to_dict
 
 TESTDATADIR = os.path.join(PROJECTDIR, 'tests', 'data', 'mets', 'techmd')
 
@@ -42,3 +42,17 @@ def test_get_tech_md_list_for_file():
         "delimiter": ";",
         "headers": ["Person name", "Person email"]}
         }
+
+
+def test_to_dict():
+    """test for get_addml_dict()."""
+    addml_path = os.path.join(TESTDATADIR, 'addml.xml')
+    addml_tree = lxml.etree.parse(addml_path)
+    expected = {"addml": {
+        "charset": "UTF-8",
+        "separator": "CR+LF",
+        "delimiter": ";",
+        "headers": ["Person name", "Person email"]}
+        }
+
+    assert to_dict(addml_tree) == expected
