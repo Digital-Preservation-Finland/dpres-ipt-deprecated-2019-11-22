1f6c935d53e536445b2f559280126994c78072db
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2016-05-18 12:23:58 +0300

    KDKPAS-1253 Fix style issues.

--- tests/scripts/test_check_sip_digital_objects.py
+++ tests/scripts/test_check_sip_digital_objects.py
@@ -18,78 +18,78 @@ METSDIR = os.path.abspath(
     os.path.join(testcommon.settings.TESTDATADIR, "mets"))
 
 TESTCASES = [{
-        "testcase": 'Test valid sip package #1',
-        "filename": 'CSC_test001',
-        "expected_result": {
-            "returncode": 0,
-            "stdout": '',
-            "stderr": ''
-        }
-    }, {
-        "testcase": 'Test valid sip package #2',
-        "filename": 'CSC_test002',
-        "expected_result": {
-            "returncode": 0,
-            "stdout": '',
-            "stderr": ''
-        }
-    }, {
-        "testcase": 'Test sip with whitespace sip package #3',
-        "filename": 'CSC whitespace',
-        "expected_result": {
-            "returncode": 0,
-            "stdout": '',
-            "stderr": ''
-        }
-    }, {
-        "testcase": 'Test valid sip package with non-existing '
-            'validator (ALTO)',
-        "filename": 'CSC_test004',
-        "expected_result": {
-            "returncode": 117,
-            "stdout": ['No validator for mimetype: text/xml version: ALTO' +
-                       ' schema Version 1.4'],
-            "stderr": []
-        }
-    }, {
-        "testcase": 'Test valid sip package #6: csc-test-valid-kdkmets-1.3',
-        "filename": 'CSC_test006',
-        "expected_result": {
-            "returncode": 0,
-            "stdout": '',
-            "stderr": ''
-        }
-    }, {
+    "testcase": 'Test valid sip package #1',
+    "filename": 'CSC_test001',
+    "expected_result": {
+        "returncode": 0,
+        "stdout": '',
+        "stderr": ''
+    }
+},
+    {
+    "testcase": 'Test valid sip package #2',
+    "filename": 'CSC_test002',
+    "expected_result": {
+        "returncode": 0,
+        "stdout": '',
+        "stderr": ''
+    }
+}, {
+    "testcase": 'Test sip with whitespace sip package #3',
+    "filename": 'CSC whitespace',
+    "expected_result": {
+        "returncode": 0,
+        "stdout": '',
+        "stderr": ''
+    }
+}, {
+    "testcase": 'Test valid sip package with non-existing '
+                'validator (ALTO)',
+    "filename": 'CSC_test004',
+    "expected_result": {
+        "returncode": 117,
+        "stdout": ['No validator for mimetype: text/xml version: ALTO' +
+            ' schema Version 1.4'],
+        "stderr": []
+    }
+}, {
+    "testcase": 'Test valid sip package #6: csc-test-valid-kdkmets-1.3',
+    "filename": 'CSC_test006',
+    "expected_result": {
+        "returncode": 0,
+        "stdout": '',
+        "stderr": ''
+    }
+}, {
 
-        "testcase": 'Unsupported file version',
-        "filename": 'CSC_test_unsupported_version',
-        "expected_result": {
-            "returncode": 117,
-            "stdout": [
-                'No validator for mimetype: application/warc version: 2.0'],
-            "stderr": ''
-        }
-    }, {
+    "testcase": 'Unsupported file version',
+    "filename": 'CSC_test_unsupported_version',
+    "expected_result": {
+        "returncode": 117,
+        "stdout": ['No validator for mimetype: application/warc version: 2.0'],
+        "stderr": ''
+    }
+}, {
 
-        "testcase": 'Unsupported file mimetype, without version',
-        "filename": 'CSC_test_unsupported_mimetype_no_version',
-        "expected_result": {
-            "returncode": 117,
-            "stdout": [
-                'No validator for mimetype: application/kissa version: '],
-            "stderr": ''
-        }
-    }, {
+    "testcase": 'Unsupported file mimetype, without version',
+    "filename": 'CSC_test_unsupported_mimetype_no_version',
+    "expected_result": {
+        "returncode": 117,
+        "stdout": ['No validator for mimetype: application/kissa version: '],
+        "stderr": ''
+    }
+}, {
 
-        "testcase": 'Unsupported file mimetype',
-        "filename": 'CSC_test_unsupported_mimetype',
-        "expected_result": {
-            "returncode": 117,
-            "stdout": [
-                'No validator for mimetype: application/kissa version: 1.0'],
-            "stderr": ''
-        }
-    }]
+    "testcase": 'Unsupported file mimetype',
+    "filename": 'CSC_test_unsupported_mimetype',
+    "expected_result": {
+        "returncode": 117,
+        "stdout": ['No validator for mimetype: application/kissa version: 1.0'],
+        "stderr": ''
+    }
+}
+]
+
 
 @pytest.mark.usefixtures("monkeypatch_Popen")
 def test_check_sip_digital_objects():
