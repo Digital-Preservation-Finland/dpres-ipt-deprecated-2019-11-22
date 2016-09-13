ef05dbe940456834f5dfb802093bc7321afa4d00
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2016-09-13 17:54:55 +0300

    KDKPAS-971 Fix tests according to new fileinfo format. Remove stale test data path.

--- tests/audiomd/audiomd_test.py
+++ tests/audiomd/audiomd_test.py
@@ -2,20 +2,15 @@
 import os
 import lxml.etree
 
-from tests.testcommon.settings import PROJECTDIR
 from ipt.audiomd.audiomd import to_dict
 
-TESTDATADIR = os.path.join(PROJECTDIR, 'tests', 'data', 'audiomd')
+TESTDATADIR = os.path.join('tests', 'data', 'audiomd')
 
 
 def test_to_dict():
     """test for audiomd.to_dict()."""
     audiomd_path = os.path.join(TESTDATADIR, 'audiomd.xml')
     audiomd_tree = lxml.etree.parse(audiomd_path)
-    expected = {
-        "audiomd": {
-            "codecname": "MPEG 2"
-        }
-    }
+    expected = {"audio": [{"codecname": "MPEG 2"}]}
 
     assert to_dict(audiomd_tree) == expected
--- tests/videomd/videomd_test.py
+++ tests/videomd/videomd_test.py
@@ -2,20 +2,15 @@
 import os
 import lxml.etree
 
-from tests.testcommon.settings import PROJECTDIR
 from ipt.videomd.videomd import to_dict
 
-TESTDATADIR = os.path.join(PROJECTDIR, 'tests', 'data', 'videomd')
+TESTDATADIR = os.path.join('tests', 'data', 'videomd')
 
 
 def test_to_dict():
     """test for videomd.to_dict()."""
     audiomd_path = os.path.join(TESTDATADIR, 'videomd.xml')
     audiomd_tree = lxml.etree.parse(audiomd_path)
-    expected = {
-        "videomd": {
-            "codecname": "MPEG 2"
-        }
-    }
+    expected = {"video": [{"codecname": "MPEG 2"}]}
 
     assert to_dict(audiomd_tree) == expected
