cbd474402edafedeef313de00aee6cd4e15a8ae9
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2016-09-27 12:41:46 +0300

    KDKPAS-971 Update test data according to new specs.

--- tests/videomd/videomd_test.py
+++ tests/videomd/videomd_test.py
@@ -11,6 +11,14 @@ def test_to_dict():
     """test for videomd.to_dict()."""
     audiomd_path = os.path.join(TESTDATADIR, 'videomd.xml')
     audiomd_tree = lxml.etree.parse(audiomd_path)
-    expected = {"video": [{"codecname": "MPEG 2"}]}
+    expected = {'video': [
+        {'avg_frame_rate': '24',
+         'level': '8',
+         'height': '480',
+         'width': '640',
+         'sample_aspect_ratio': '4:3',
+         'display_aspect_ratio': '4/3',
+         'codec_name': 'MPEG 2',
+         'duration': '01:31:37'}]}
 
     assert to_dict(audiomd_tree) == expected
