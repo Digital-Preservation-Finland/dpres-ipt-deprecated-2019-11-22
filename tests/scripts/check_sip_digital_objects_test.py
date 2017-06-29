c92f62f72da32ee81129301d2283c7d3abac6088
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2017-06-29 11:45:29 +0300

    KDKPAS-1261 Removed unnecessary test data

--- tests/scripts/check_sip_digital_objects_test.py
+++ tests/scripts/check_sip_digital_objects_test.py
@@ -84,14 +84,6 @@ TESTCASES = [
                     'warc errors at',
                     'File version check error'],
          "stderr": ''}},
-    {"testcase": 'Invalid gzipped warc',
-     "filename": 'csc-test-invalid-warc-gz',
-     "expected_result": {
-         "returncode": 117,
-         "stdout": ['Validation failed: returncode 255',
-                    'warc errors at',
-                    'File version check error'],
-         "stderr": ''}},
     {"testcase": 'Invalid warc renamed to .gz',
      "filename": 'csc-test-invalid-warc-not-gz',
      "expected_result": {
